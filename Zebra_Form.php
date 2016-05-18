468c98c544ad5d60f0bcf528cda6bf233ebd0ef4
Stefan Gabos <contact@stefangabos.ro>
2016-05-18 16:03:50 +0300

    Updated version number to 2.9.8

--- Zebra_Form.php
+++ Zebra_Form.php
@@ -27,7 +27,7 @@ define('ZEBRA_FORM_UPLOAD_RANDOM_NAMES', false);
  *  For more resources visit {@link http://stefangabos.ro/}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    2.9.8 (last revision: May 16, 2016)
+ *  @version    2.9.8 (last revision: May 18, 2016)
  *  @copyright  (c) 2006 - 2016 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Form
