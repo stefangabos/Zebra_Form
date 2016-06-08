67e92e1fcde0ebf0be05e6cb402b4824ab9bc282
Stefan Gabos <contact@stefangabos.ro>
2016-06-08 16:26:40 +0300

    Added Japanese language file; thanks saltymouse!

---
+++ languages/japanese.php
@@ -0,0 +1,18 @@
+<?php
+
+$this->language = array(
+
+    'clear_date'    => '日付を消す',
+    'csrf_detected' => '送信に問題がありました。送信するまでの時間が長すぎるかもしれません。もしくは今送った送信リクエストが重複している為こちらのメッセージが表示されているかもしれません。また送信して下さい',
+    'days'          => array('日','月','火','水','木','金','土'),
+    'days_abbr'     => false,   // Japanese has different abbreviations (used above) so this is set to false
+    'months'        => array('１月','２月','３月','４月','５月','６月','７月','８月','９月','１０月','１１月','１２月'),
+    'months_abbr'   => false,   // Japanese has no month abbreviations
+    'new_captcha'   => '新しい承認記号に変える',
+    'other'         => 'その他...',
+    'select'        => '《 選択して下さい 》',
+    'spam_detected' => '先程送信したメッセージはスパム行為の恐れがありましたので、送信しませんでした',
+    'today'         => '今日',
+);
+
+?>
