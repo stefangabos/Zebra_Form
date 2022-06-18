2b8667741c433b615aae08dc952fa4b4eeeec61d
stefangabos <contact@stefangabos.ro>
2022-06-18 17:28:18 +0300

    Refactored code and updated documentation for the CAPTCHA element

--- includes/Captcha.php
+++ includes/Captcha.php
@@ -1,123 +1,116 @@
 <?php
 
 /**
- *  Class for CAPTCHA controls.
+ *  Creates {@link https://en.wikipedia.org/wiki/CAPTCHA CAPTCHA} form elements
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @copyright  (c) 2006 - 2016 Stefan Gabos
- *  @package    Controls
+ *  @copyright  © 2006 - 2022 Stefan Gabos
+ *  @package    Elements
  */
-
-class Zebra_Form_Captcha extends Zebra_Form_Control
-{
+class Zebra_Form_Captcha extends Zebra_Form_Shared {
 
     /**
-     *  Adds a CAPTCHA image to the form.
+     *  Adds a `CAPTCHA` image to the form.
      *
-     *  <b>Do not instantiate this class directly! Use the {@link Zebra_Form::add() add()} method instead!</b>
+     *  >   Do not instantiate this class directly!<br>
+     *      Use the {@link Zebra_Form::add() add()} method instead!
      *
-     *  <b>You must also place a {@link Zebra_Form_Text textbox} control on the form and set the "captcha" rule to it!
-     *  (through {@link set_rule()})</b>
+     *  >   You must also place a {@link Zebra_Form_Text text input} element on the form and configure the `captcha`
+     *      rule for it via {@link set_rule()}.
      *
-     *  Properties of the CAPTCHA image can be altered by editing the file includes/captcha.php.
+     *  Default properties of the CAPTCHA image can be altered by editing the file `process.php`.
      *
-     *  By default, captcha values are triple md5 hashed and stored in cookies, and when the user enters the captcha
-     *  value the value is also triple md5 hashed and the two values are then compared. Sometimes, your users may have
-     *  a very restrictive cookie policy and so cookies will not be set, and therefore they will never be able to get
-     *  past the CAPTCHA control. If it's the case, call the {@link Zebra_Form::captcha_storage() captcha_storage}
-     *  method and set the storage method to "session".
+     *  Captcha values are hashed and stored, by default, in `cookies`. When the user enters the captcha value, the entered
+     *  value is also hashed and then compared against the stored value. Users may have restrictive cookie policies and
+     *  cookies might not be allowed, resulting in users never being able to get past the CAPTCHA test. If so, use
+     *  {@link Zebra_Form::captcha_storage() captcha_storage} and set the storage method to `session`.
      *
      *  <code>
+     *
      *  // create a new form
      *  $form = new Zebra_Form('my_form');
      *
      *  // add a CAPTCHA image
-     *  $form->add('captcha', 'my_captcha', 'my_text');
+     *  $form->add('captcha', 'my_captcha', 'user_input');
      *
-     *  // add a label for the textbox
-     *  $form->add('label', 'label_my_text', 'my_text', 'Are you human?');
+     *  // add a label for the text input
+     *  $form->add('label', 'label_user_input', 'user_input', 'Are you a human?');
      *
-     *  // add a CAPTCHA to the form
-     *  $obj = $form->add('text', 'my_text');
+     *  // add a text input where the user will input the
+     *  // characters shown in the CAPTCHA image
+     *  $element = $form->add('text', 'user_input');
      *
-     *  // set the "captcha" rule to the textbox
-     *  $obj->set_rule(array(
+     *  // set the "captcha" rule
+     *  $element->set_rule(array(
      *      'captcha' => array('error', 'Characters not entered correctly!')
      *  ));
      *
-     *  // don't forget to always call this method before rendering the form
+     *  // this method needs to be called before rendering the form
      *  if ($form->validate()) {
-     *      // put code here
+     *
+     *      // do stuff here
+     *
      *  }
      *
-     *  // output the form using an automatically generated template
-     *  $form->render();
+     *  // generate the form
+     *  $output = $form->render('my-template', true);
+     *
      *  </code>
      *
-     *  @param  string  $id             Unique name to identify the control in the form.
+     *  @param  string  $id             Unique name to identify the element in the form.
      *
-     *                                  This is the name of the variable to be used in the template file, containing
-     *                                  the generated HTML for the control.
+     *                                  >   The name must resolve to a valid `PHP` variable. As per PHP documentation, a
+     *                                      valid variable name starts with a letter or underscore, followed by any number
+     *                                      of letters, numbers, or underscores
+     *
+     *                                  This is the name of the variable to be used in the template file for displaying
+     *                                  the element.
      *
      *                                  <code>
-     *                                  // in a template file, in order to print the generated HTML
-     *                                  // for a control named "my_captcha", one would use:
+     *                                  // in a template file, in order to output the element's HTML code
+     *                                  // for an element named "my_captcha", one would use:
      *                                  echo $my_captcha;
      *                                  </code>
      *
-     *  @param  string  $attach_to      The <b>id</b> attribute of the {@link Zebra_Form_Text textbox} control to attach
-     *                                  the CAPTCHA image to.
+     *  @param  string  $input_element  The `id` attribute of an {@link Zebra_Form_Text text input} element to associate
+     *                                  the CAPTCHA image with.
      *
      *  @return void
      */
-    function __construct($id, $attach_to, $storage = 'cookie')
-    {
+    function __construct($id, $input_element) {
 
         // call the constructor of the parent class
         parent::__construct();
-        
-        // set the private attributes of this control
-        // these attributes are private for this control and are for internal use only
-        // and will not be rendered by the _render_attributes() method
-        $this->private_attributes = array(
 
+        // set private attributes, for internal use only
+        // (will not be rendered by the _render_attributes() method)
+        $this->private_attributes = array(
             'disable_xss_filters',
             'for',
             'locked',
-
         );
 
-        // set the default attributes for the text control
-        // put them in the order you'd like them rendered
-        $this->set_attributes(
-        
-            array(
-            
-                'type'      =>  'captcha',
-                'name'      =>  $id,
-                'id'        =>  $id,
-                'for'       =>  $attach_to,
-
-            )
-            
-        );
+        // set the default attributes
+        $this->set_attributes(array(
+            'type'  =>  'captcha',
+            'name'  =>  $id,
+            'id'    =>  $id,
+            'for'   =>  $input_element,
+        ));
 
     }
 
     /**
-     *  Generates the control's HTML code.
+     *  Generates the form element's HTML code.
      *
-     *  <i>This method is automatically called by the {@link Zebra_Form::render() render()} method!</i>
+     *  >   This method is automatically called by the {@link Zebra_Form::render() render()} method.
      *
-     *  @return string  The control's HTML code
+     *  @return string  Returns the form element's generated HTML code.
      */
-    function toHTML()
-    {
+    function toHTML() {
 
-        return '<div class="captcha-container"><img src="' . $this->form_properties['assets_url'] . 'process.php?captcha=' . ($this->form_properties['captcha_storage'] == 'session' ? 2 : 1) . '&amp;nocache=' . time() . '" alt=""' . ($this->form_properties['doctype'] == 'xhtml' ? '/' : '') . '><a href="javascript:void(0)" title="' . $this->form_properties['language']['new_captcha'] . '">' . $this->form_properties['language']['new_captcha'] . '</a></div>';
-    
+        return '<div class="zebra-form-captcha-container"><img src="' . $this->form_properties['assets_url'] . 'process.php?captcha=' . ($this->form_properties['captcha_storage'] == 'session' ? 2 : 1) . '&amp;nocache=' . time() . '" alt=""' . ($this->form_properties['doctype'] == 'xhtml' ? '/' : '') . '><a href="javascript:void(0)" title="' . $this->form_properties['language']['new_captcha'] . '">' . $this->form_properties['language']['new_captcha'] . '</a></div>';
+
     }
-    
-}
 
-?>
+}
