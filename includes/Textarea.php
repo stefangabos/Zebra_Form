1df01cf72d2488f77fc26ca5d8c9e695ec54410c
stefangabos <contact@stefangabos.ro>
2022-07-03 15:35:31 +0300

    Code refactoring

--- includes/Textarea.php
+++ includes/Textarea.php
@@ -1,62 +1,75 @@
 <?php
 
 /**
- *  Class for textarea controls
+ *  Create `<textarea>` form elements
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @copyright  (c) 2006 - 2016 Stefan Gabos
- *  @package    Controls
+ *  @copyright  © 2006 - 2022 Stefan Gabos
+ *  @package    Elements
  */
-class Zebra_Form_Textarea extends Zebra_Form_Control
-{
+class Zebra_Form_Textarea extends Zebra_Form_Shared {
 
     /**
-     *  Adds an <textarea> control to the form.
+     *  Create `<textarea>` form elements.
      *
-     *  <b>Do not instantiate this class directly! Use the {@link Zebra_Form::add() add()} method instead!</b>
+     *  >   Do not instantiate this class directly!<br>
+     *      Use the {@link Zebra_Form::add() add()} method instead!
      *
      *  <code>
      *  // create a new form
      *  $form = new Zebra_Form('my_form');
      *
-     *  // add a textarea control to the form
-     *  $obj = $form->add('textarea', 'my_textarea');
+     *  // add a label
+     *  $form->add('label', 'label_my_textarea', 'my_textarea', 'Type something');
      *
-     *  // don't forget to always call this method before rendering the form
+     *  // add a textarea control to the form
+     *  $element = $form->add('textarea', 'my_textarea');
+     *
+     *  // make it required
+     *  $element->set_rule(array(
+     *      'required'  =>  array('error', 'You must type something!'),
+     *  ));
+     *
+     *  // this method needs to be called before rendering the form
      *  if ($form->validate()) {
-     *      // put code here
+     *
+     *      // do stuff
+     *
      *  }
      *
-     *  // output the form using an automatically generated template
-     *  $form->render();
+     *  // generate the form
+     *  $output = $form->render('my-template', true);
+     *
      *  </code>
      *
-     *  @param  string  $id             Unique name to identify the control in the form.
+     *  @param  string  $id             Unique name to identify the element in the form.
      *
-     *                                  The control's <b>name</b> attribute will be the same as the <b>id</b> attribute!
+     *                                  The element's `name` attribute will be the same as the `id` attribute.
      *
-     *                                  This is the name to be used when referring to the control's value in the
-     *                                  POST/GET superglobals, after the form is submitted.
+     *                                  This is the name to be used for accessing the element's value in {@link https://www.php.net/manual/en/reserved.variables.post.php $_POST} /
+     *                                  {@link https://www.php.net/manual/en/reserved.variables.get.php $_GET}, after the
+     *                                  form is submitted.
      *
-     *                                  This is also the name of the variable to be used in custom template files, in
-     *                                  order to display the control.
+     *                                  This is also the name of the variable to be used in the template file for
+     *                                  displaying the element.
      *
      *                                  <code>
-     *                                  // in a template file, in order to print the generated HTML
-     *                                  // for a control named "my_textarea", one would use:
+     *                                  // in a template file, in order to output the element's HTML code
+     *                                  // for an element named "my_textarea", one would use:
      *                                  echo $my_textarea;
      *                                  </code>
      *
      *  @param  string  $default        (Optional) Default value of the textarea.
      *
      *  @param  array   $attributes     (Optional) An array of attributes valid for
-     *                                  <b>{@link http://www.w3.org/TR/REC-html40/interact/forms.html#h-17.7 textarea}</b>
-     *                                  controls (rows, cols, style, etc)
+     *                                  {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/textarea textarea}
+     *                                  form elements (like `disabled`, `readonly`, `style`, etc.).
      *
-     *                                  Must be specified as an associative array, in the form of <i>attribute => value</i>.
+     *                                  Must be specified as an associative array, in the form of *attribute => value*.
+     *
      *                                  <code>
      *                                  // setting the "rows" attribute
-     *                                  $obj = $form->add(
+     *                                  $form->add(
      *                                      'textarea',
      *                                      'my_textarea',
      *                                      '',
@@ -66,54 +79,41 @@ class Zebra_Form_Textarea extends Zebra_Form_Control
      *                                  );
      *                                  </code>
      *
-     *                                  See {@link Zebra_Form_Control::set_attributes() set_attributes()} on how to set
-     *                                  attributes, other than through the constructor.
+     *                                  Attributes may also be set after the form element is created with the
+     *                                  {@link Zebra_Form_Shared::set_attributes() set_attributes()} method.
      *
-     *                                  The following attributes are automatically set when the control is created and
-     *                                  should not be altered manually:<br>
-     *
-     *                                  <b>id</b>, <b>name</b>, <b>class</b>
+     *                                  The following attributes are automatically set when the form element is created
+     *                                  and should not be altered manually: `id`, `name`.
      *
      *  @return void
      */
-    function __construct($id, $default = '', $attributes = '')
-    {
-    
+    function __construct($id, $default = '', $attributes = '') {
+
         // call the constructor of the parent class
         parent::__construct();
-    
-        // set the private attributes of this control
-        // these attributes are private for this control and are for internal use only
-        // and will not be rendered by the _render_attributes() method
-        $this->private_attributes =  array(
 
+        // set private attributes, for internal use only
+        // (will not be rendered by the _render_attributes() method)
+        $this->private_attributes =  array(
             'default_value',
             'disable_xss_filters',
             'locked',
             'type',
             'value',
-
 		);
 
-        // set the default attributes for the textarea control
-        // put them in the order you'd like them rendered
-        $this->set_attributes(
+        // set the default attributes
+        $this->set_attributes(array(
+            'name'      =>  $id,
+            'id'        =>  $id,
+            'rows'      =>  5,
+            'cols'      =>  '80',                   // used for passing W3C validation
+            'class'     =>  'zebraform-control',
+            'type'      =>  'textarea',
+            'value'     =>  $default,
+        ));
 
-            array(
-
-                'name'      =>  $id,
-                'id'        =>  $id,
-                'rows'      =>  5,
-                'cols'      =>  '80',           // used only for passing W3C validation
-                'class'     =>  'control',
-                'type'      =>  'textarea',
-                'value'     =>  $default,
-
-            )
-
-        );
-        
-        // if "class" is amongst user specified attributes
+        // if "class" is among user specified attributes
         if (is_array($attributes) && isset($attributes['class'])) {
 
             // we need to set the "class" attribute like this, so it doesn't overwrite previous values
@@ -124,20 +124,19 @@ class Zebra_Form_Textarea extends Zebra_Form_Control
 
         }
 
-        // sets user specified attributes for the control
+        // set user specified attributes
         $this->set_attributes($attributes);
-        
+
     }
-    
+
     /**
-     *  Generates the control's HTML code.
+     *  Generates the form element's HTML code.
      *
-     *  <i>This method is automatically called by the {@link Zebra_Form::render() render()} method!</i>
+     *  >   This method is automatically called by the {@link Zebra_Form::render() render()} method.
      *
-     *  @return string  The control's HTML code
+     *  @return string  Returns the form element's generated HTML code.
      */
-    function toHTML()
-    {
+    function toHTML() {
 
         // get private attributes
         $attributes = $this->get_attributes('value');
@@ -147,5 +146,3 @@ class Zebra_Form_Textarea extends Zebra_Form_Control
     }
 
 }
-
-?>
