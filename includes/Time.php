6dced5d42ac1b1cb8bc8c0d52b614c3febab81da
stefangabos <contact@stefangabos.ro>
2022-07-08 22:51:45 +0300

    Code refactoring

--- includes/Time.php
+++ includes/Time.php
@@ -1,34 +1,42 @@
 <?php
 
 /**
- *  Class for time picker controls.
+ *  Create `time picker` form elements
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @copyright  (c) 2006 - 2016 Stefan Gabos
- *  @package    Controls
+ *  @copyright  (c) 2006 - 2022 Stefan Gabos
+ *  @package    Elements
  */
-class Zebra_Form_Time extends Zebra_Form_Control
-{
+class Zebra_Form_Time extends Zebra_Form_Shared {
 
     /**
-     *  Adds a time picker control to the form.
+     *  Adds a `time picker` element to the form.
      *
-     *  <b>Do not instantiate this class directly! Use the {@link Zebra_Form::add() add()} method instead!</b>
+     *  >   Do not instantiate this class directly!<br>
+     *      Use the {@link Zebra_Form::add() add()} method instead!
      *
-     *  The output of this control will be one, two, three or four {@link Zebra_Form_Select select} controls for hour,
-     *  minutes, seconds and AM/PM respectively, according to the given format as set by the <i>$attributes</i> argument.
+     *  The output of this element will be one, two, three or four {@link Zebra_Form_Select select} elements, for hour,
+     *  minutes, seconds and AM/PM respectively, according to the given format as set through the `format` attribute.
      *
      *  Note that even though there will be more select boxes, the submitted values will be available as a single merged
-     *  value (in the form of hh:mm:ss AM/PM, depending on the format), with the name as given by the <i>id</i> argument.
+     *  value (in the form of hh:mm:ss AM/PM, depending on the format), with the name as defined through the `id` argument.
      *
      *  <code>
      *  // create a new form
      *  $form = new Zebra_Form('my_form');
      *
-     *  // add a time picker control for hour and minutes
-     *  $obj = $form->add('time', 'my_time', date('H:i'), array('format' => 'hm'));
+     *  // add a label
+     *  $form->add('label', 'label_my_time', 'my_time', 'Pick a time');
      *
-     *  // don't forget to always call this method before rendering the form
+     *  // add a time picker element, for hour and minutes
+     *  $element = $form->add('time', 'my_time', date('H:i'), array('format' => 'hm'));
+     *
+     *  // make it required
+     *  $element->set_rule(array(
+     *      'required'  =>  array('error', 'You must pick a time!'),
+     *  ));
+     *
+     *  // this method needs to be called before rendering the form
      *  if ($form->validate()) {
      *
      *      // note that even though there will be more select boxes, the submitted
@@ -39,65 +47,66 @@ class Zebra_Form_Time extends Zebra_Form_Control
      *
      *  }
      *
-     *  // output the form using an automatically generated template
-     *  $form->render();
+     *  // generate the form
+     *  $output = $form->render('my-template', true);
+     *
      *  </code>
      *
-     *  @param  string  $id             Unique name to identify the control in the form.
+     *  @param  string  $id             Unique name to identify the element in the form.
      *
-     *                                  The control's <b>name</b> attribute will be the same as the <b>id</b> attribute!
+     *                                  The element's `name` attribute will be the same as the `id` attribute.
      *
-     *                                  This is the name to be used when referring to the control's value in the
-     *                                  POST/GET superglobals, after the form is submitted.
+     *                                  This is the name to be used for accessing the element's value in {@link https://www.php.net/manual/en/reserved.variables.post.php $_POST} /
+     *                                  {@link https://www.php.net/manual/en/reserved.variables.get.php $_GET}, after the
+     *                                  form is submitted.
      *
-     *                                  This is also the name of the variable to be used in custom template files, in
-     *                                  order to display the control.
+     *                                  This is also the name of the variable to be used in the template file for
+     *                                  displaying the element.
      *
      *                                  <code>
-     *                                  // in a template file, in order to print the generated HTML
-     *                                  // for a control named "my_time", one would use:
+     *                                  // in a template file, in order to output the element's HTML code
+     *                                  // for an element named "my_time", one would use:
      *                                  echo $my_time;
      *                                  </code>
      *
      *  @param  string  $default        (Optional) String representing the default time to be shown. Must be set according
-     *                                  to the format of the time, as specified in <i>$attributes</i>. For example, for a
-     *                                  time format of "hm", one would set the default time in the form of "hh:mm" while
-     *                                  for a time format of "hms", one would set the time in the form of "hh:mm:ss".
+     *                                  to the format of the time, as specified through the `format` attribute. For example,
+     *                                  for a time format of "hm", one would set the default time in the form of "hh:mm"
+     *                                  while for a time format of "hms", one would set the time in the form of "hh:mm:ss".
      *
      *                                  Default is current system time.
      *
-     *  @param  array   $attributes     (Optional) An array of user specified attributes valid for an time picker
-     *                                  control (format, hours, minutes, seconds, am/pm).
+     *  @param  array   $attributes     (Optional) An array of user specified attributes valid for a time picker
+     *                                  element (format, hours, minutes, seconds, am/pm).
      *
-     *                                  Must be specified as an associative array, in the form of <i>attribute => value</i>.
+     *                                  Must be specified as an associative array, in the form of *attribute => value*.
      *
      *                                  Available attributes are:
      *
-     *                                  -   format - format of time; a string containing one, or a combination of the four
-     *                                      allowed characters: "h" (hours), "m" (minutes) and "s" (seconds) and "g" for
-     *                                      using 12-hours format instead of the default 23-hours format; (i.e. setting the
-     *                                      format to "hm" would allow the selection of hours and minutes, setting the
-     *                                      format to "hms" would allow the selection of hours, minutes and seconds, and
-     *                                      setting the format to "hmg" would allow the selection of hours and minutes
-     *                                      using the 12-hours format instead of the 24-hours format)
+     *                                  -   `format` - format of time; a string a combination of the four allowed characters:
+     *                                      `h` (for hours), `m` (for minutes), `s` (for seconds) and `g` for using 12-hours
+     *                                      format instead of the default 24-hours format; (i.e. setting the format to `hm`
+     *                                      would allow the selection of hours and minutes, setting the format to `hms`
+     *                                      would allow the selection of hours, minutes and seconds, and setting the format
+     *                                      to `hmg` would allow the selection of hours and minutes using the 12-hours
+     *                                      format instead of the default 24-hours format).
      *
-     *                                  -   hours - an array of selectable hours (i.e. array(10, 11, 12))
+     *                                  -   `hours` - an array of selectable hours (i.e. `array(10, 11, 12)`)
      *
-     *                                  -   minutes - an array of selectable minutes (i.e. array(15, 30, 45))
+     *                                  -   `minutes` - an array of selectable minutes (i.e. `array(15, 30, 45)`)
      *
-     *                                  -   seconds - an array of selectable seconds
+     *                                  -   `seconds` - an array of selectable seconds
      *
-     *                                  See {@link Zebra_Form_Control::set_attributes() set_attributes()} on how to set
-     *                                  attributes, other than through the constructor.
+     *                                  Attributes may also be set after the form element is created with the
+     *                                  {@link Zebra_Form_Shared::set_attributes() set_attributes()} method.
      *
      *  @return void
      */
-    function __construct($id, $default = '', $attributes = '')
-    {
-    
+    function __construct($id, $default = '', $attributes = '') {
+
         // call the constructor of the parent class
         parent::__construct();
-    
+
         // these will hold the default selectable hours, minutes and seconds
         $hours = $minutes = $seconds = array();
 
@@ -107,11 +116,9 @@ class Zebra_Form_Time extends Zebra_Form_Control
         // all the minutes and seconds are available by default
         for ($i = 0; $i < 60; $i++) $minutes[] = $seconds[] = $i;
 
-        // set the private attributes of this control
-        // these attributes are private for this control and are for internal use only
-        // and will not be rendered by the _render_attributes() method
+        // set private attributes, for internal use only
+        // (will not be rendered by the _render_attributes() method)
         $this->private_attributes = array(
-        
             'disable_xss_filters',
             'locked',
             'type',
@@ -122,45 +129,36 @@ class Zebra_Form_Time extends Zebra_Form_Control
             'minutes',
             'seconds',
             'value',
-            
         );
 
-        // set the default attributes for the text control
-        // put them in the order you'd like them rendered
-        $this->set_attributes(
-        
-            array(
-            
-                'type'      =>  'time',
-                'name'      =>  $id,
-                'id'        =>  $id,
-                'value'     =>  $default,
-                'class'     =>  'control time',
-                'format'    =>  'hm',
-                'hours'     =>  $hours,
-                'minutes'   =>  $minutes,
-                'seconds'   =>  $seconds,
+        // set the default attributes
+        $this->set_attributes(array(
+            'type'      =>  'time',
+            'name'      =>  $id,
+            'id'        =>  $id,
+            'value'     =>  $default,
+            'class'     =>  'zebraform-control zebraform-time',
+            'format'    =>  'hm',
+            'hours'     =>  $hours,
+            'minutes'   =>  $minutes,
+            'seconds'   =>  $seconds,
+        ));
 
-            )
-            
-        );
-
-        // sets user specified attributes for the control
+        // set user specified attributes
         $this->set_attributes($attributes);
-        
+
     }
 
     /**
-     *  Generates and returns the control's HTML code.
+     *  Generates the form element's HTML code.
      *
-     *  <i>This method is automatically called by the {@link Zebra_Form::render() render()} method!</i>
+     *  >   This method is automatically called by the {@link Zebra_Form::render() render()} method.
      *
-     *  @return string  The control's HTML code
+     *  @return string  Returns the form element's generated HTML code.
      */
-    function toHTML()
-    {
+    function toHTML() {
 
-        // get some attributes of the control
+        // get some attributes of the element
         $attributes = $this->get_attributes(array('name', 'value', 'class', 'format', 'hours', 'minutes', 'seconds'));
 
         // make sure format is in lower characters
@@ -169,7 +167,7 @@ class Zebra_Form_Time extends Zebra_Form_Control
         // if invalid format specified, revert to the default "hm"
         if (preg_match('/^[hmsg]+$/i', $attributes['format']) == 0 || strlen(preg_replace('/([a-z]{2,})/i', '$1', $attributes['format'])) != strlen($attributes['format'])) $attributes['format'] = 'hm';
 
-        // see what have we sepcified as default time
+        // see what have we specified as default time
         $time = array_diff(explode(':', trim(str_replace(array('am', 'pm'), '', strtolower($attributes['value'])))), array(''));
 
         // if, according to the time format, we have to show the hours, and the hour is given in the default time
@@ -227,7 +225,7 @@ class Zebra_Form_Time extends Zebra_Form_Control
             ';
 
         }
-        
+
         // if the minute picker is to be shown
         if ($minutes_position !== false) {
 
@@ -237,7 +235,6 @@ class Zebra_Form_Time extends Zebra_Form_Control
                     <option value="">-</option>';
 
             foreach ($attributes['minutes'] as $minute)
-
                 $output .= '<option value="' . str_pad($minute, 2, '0', STR_PAD_LEFT) . '"' . (isset($selected_minute) && ltrim($selected_minute, '0') == ltrim($minute, '0') ? ' selected="selected"' : '') . '>' . str_pad($minute, 2, '0', STR_PAD_LEFT) . '</option>';
 
             $output .= '
@@ -255,7 +252,6 @@ class Zebra_Form_Time extends Zebra_Form_Control
                     <option value="">-</option>';
 
             foreach ($attributes['seconds'] as $second)
-
                 $output .= '<option value="' . str_pad($second, 2, '0', STR_PAD_LEFT) . '"' . (isset($selected_second) && ltrim($selected_second, '0') == ltrim($second, '0') ? ' selected="selected"' : '') . '>' . str_pad($second, 2, '0', STR_PAD_LEFT) . '</option>';
 
             $output .= '
@@ -288,5 +284,3 @@ class Zebra_Form_Time extends Zebra_Form_Control
     }
 
 }
-
-?>
